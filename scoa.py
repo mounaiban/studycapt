31695fae7fc5818796fee7ae6d4cd2f102daeb1c
mounaiban <29518031+mounaiban@users.noreply.github.com>
2022-05-19 22:10:25 +1000

    Add scoa.py module and unit tests
    scoa.py is the SCoA Toolkit for Python for studying the SCoA codec
    
    scoa.py is not working correctly yet and is for public review only at this stage

---
+++ scoa.py
@@ -0,0 +1,233 @@
+"""
+SCoA Toolkit for Python
+"""
+# Written by Moses Chong
+# First edition 2022/05/16
+#
+# PUBLIC DOMAIN, NO RIGHTS RESERVED
+#
+# To the extent possible under law, the author(s) have dedicated all
+# copyright and related and neighboring rights to this software
+# to the public domain worldwide. This software is distributed
+# without any warranty.
+#
+# You should have received a copy of the CC0 Public Domain Dedication
+# along with this software. If not, see:
+# <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+# NOTES
+# =====
+# * This module only deals with compression, an upcoming, separate
+#   module will handle print data extraction from CAPT job files.
+# 
+# * The decompression routine in SCoADecoder.decode() is still not
+#   quite correct; investigations and remedies are on the way...
+#
+import pdb
+from itertools import chain
+
+SCOA_OLD_NEW = 0b00_000000 # uncompressed bytes (old+new)
+SCOA_OLD_REPEAT = 0b01_000000 # compressed bytes (old+repeat)
+SCOA_REPEAT_NEW = 0b11_000000 # compressed + uncompressed bytes (repeat+new)
+# CopyLong commands; SCOA_LO opcodes must come after SCOA_LONG_OLDB [_248]
+SCOA_LONG_OLDB = 0b100_00000
+SCOA_LONG_OLDB_248 = 0x9F
+SCOA_LO_NEWB = 0b00_000000
+SCOA_LO_REPEAT = 0b10_000000
+# RepeatLong commands; SCOA_LR opcodes must come after SCOA_LONG_REPEAT
+SCOA_LONG_REPEAT = 0b101_00000
+SCOA_LR_LONG_NEW_ONLY = 0b11_000000
+SCOA_LR_ONLY = 0b10_000000
+SCOA_LR_NEWB = 0b00_000000
+# Control commands
+SCOA_NOP = 0x40
+SCOA_EOL = 0x41
+SCOA_EOP = 0x42
+
+class SCoADecoder:
+    """
+    SCoA Decoder Object to decompress SCoA streams. SCoA streams
+    encode 1-bit rasters very similar in spec to the PBM P4 format.
+
+    """
+    UINT_3_MASK_HI = 0b00_111_000
+    UINT_3_MASK_LO = 0b00_000_111
+    UINT_5_MASK = 0b000_11111
+
+    def __init__(self, line_size, **kwargs):
+        """
+        Create an SCoA decoder object.
+
+        The ``line_size`` argument sets the byte length of the output
+        bitmap (coincidentially ceiling of pixels/8).
+
+        Keyword Arguments
+        -----------------
+        * init_value: fill the buffer with this repeating single-byte
+          pattern.
+
+        """
+        if type(line_size) is not int: raise TypeError('line_size must be int')
+        initv = kwargs.get('init_value', b'\x00')
+        # validate init value
+        if type(initv) is not bytes:
+            raise TypeError('init_value must be a single byte')
+        elif len(initv) > 1:
+            raise ValueError('init_value must be a single byte')
+
+        self.line_size = line_size
+        self._init_value = initv
+        self._buffer = [ord(initv),] * self.line_size
+        self._i_buf = 0 # indices are in the object, because this allows
+        self._i_in = 0  # monitoring to enable progress reports
+
+    # the order is always old, repeat, new
+    # for example no opcode puts new before old or repeat...
+    # thus, a VLIW-like approach has been taken where the operations
+    # always run and in this order...
+    # parts that are not needed are dummied out.
+
+    def _writeout(self, np=0, nr=0, rb=(), ub=()):
+        """
+        Return an generator of the expanded form of an SCoA opcode/packet.
+
+        * np: number of old bytes from prev line
+
+        * nr: number of repeated new bytes
+
+        * rb: byte to repeat
+
+        * ub: iter of uncompressed new bytes
+
+        TODO: Mismatched line_sizes may lead to StopIteration errors.
+        This error condition is yet to be properly handled.
+
+        """
+        iterold = (x for x in self._buffer[self._i_buf : self._i_buf+np])
+        iterrep = (x for x in rb*nr)
+        iternew = (x for x in ub)
+        return chain(iterold, iterrep, iternew)
+
+    def decode(self, biter, debug=False):
+        """
+        Decompress an iter yielding bytes from an SCoA-compressed
+        stream ``biter``.
+
+        Return a generator yielding uncompressed bytes.
+
+        NOTE: The decoder is still not quite correct
+
+        Example
+        -------
+        decoder = SCoADecoder(596)    # A4 width
+        file_h = open('page-1.scoa.bin', mode='rb')
+        decoder_iter = decode(iter(file_h.read()))
+        decoded_bytes = bytes(x for x in decoder_iter)
+
+        An iter is used to avoid having to read entire streams into
+        large buffers.
+
+        """
+        current_buf = [self._init_value,] * self.line_size
+        self._i_in = 0
+        np = 0 # number of bytes from previous line
+        npx = 0 # number of 0x9f opcodes (np, extended)
+        nr = 0 # number of bytes to repeat
+        rb = () # repeating byte (must still be a tuple due to implementation)
+        ub = () # uncompressed byte(s)
+        #
+        # parsing (like opcode decode)
+        for b in biter:
+            if b == SCOA_NOP:
+                pass
+            elif b == SCOA_EOL:
+                np = self.line_size - self._i_buf
+            elif b == SCOA_EOP:
+                return
+                # raise StopIteration
+            # two-bit opcodes
+            elif b & 0xC0 == SCOA_OLD_NEW:
+                np = (b & self.UINT_3_MASK_LO)
+                n_new = (b & self.UINT_3_MASK_HI) >> 3
+                ub = (next(biter) for i in range(n_new))
+                self._i_in += n_new
+            elif b & 0xC0 == SCOA_OLD_REPEAT:
+                np = (b & self.UINT_3_MASK_LO)
+                nr = (b & self.UINT_3_MASK_HI) >> 3
+                rb = (next(biter),)
+                self._i_in += 1
+            elif b & 0xC0 == SCOA_REPEAT_NEW:
+                nr = (b & self.UINT_3_MASK_HI) >> 3
+                rb = (next(biter),)
+                n_new = b & self.UINT_3_MASK_LO
+                ub = (next(biter) for i in range(n_new))
+                self._i_in += n_new
+            # three-bit opcodes with two-bit subcommand opcode
+            elif b & 0xE0 == SCOA_LONG_OLDB:
+                while b == SCOA_LONG_OLDB_248:
+                    npx += 1
+                    b = next(biter)
+                # TODO: understanding of old_Long + new may be wrong
+                np = (b & self.UINT_5_MASK) << 3
+                nextb = next(biter)
+                np |= nextb & self.UINT_3_MASK_LO
+                if nextb & 0xC0 == SCOA_LO_NEWB:
+                    n_new = b & self.UINT_3_MASK_LO
+                    # n_new = (b & self.UINT_3_MASK_HI) >> 3
+                    ub = (next(biter) for i in range(n_new))
+                    self._i_in += n_new
+                elif nextb & 0xC0 == SCOA_LO_REPEAT:
+                    nr = b & self.UINT_3_MASK_LO
+                    #nr = (b & self.UINT_3_MASK_HI) >> 3
+                    rb = (next(biter),)
+                    self._i_in += 1
+            elif b & 0xE0 == SCOA_LONG_REPEAT:
+                nr = (b & self.UINT_5_MASK) << 3
+                nextb = next(biter)
+                if nextb & 0xC0 == SCOA_LR_LONG_NEW_ONLY:
+                    n_new = nr
+                    n_new |= (nextb & self.UINT_3_MASK_HI) >> 3
+                    nr = 0
+                    ub = (next(biter) for i in range(n_new))
+                    self._i_in += n_new + 1
+                elif nextb & 0xC0 == SCOA_LR_ONLY:
+                    nr |= (nextb & self.UINT_3_MASK_HI) >> 3
+                    rb = (next(biter),)
+                    self._i_in += 2
+                elif nextb & 0xC0 == SCOA_LR_NEWB:
+                    nr |= (nextb & self.UINT_3_MASK_HI) >> 3
+                    n_new = (nextb & self.UINT_3_MASK_LO)
+                    rb = (next(biter),)
+                    ub = (next(biter) for i in range(n_new))
+                    self._i_in += n_new + 1
+            else:
+                report = {
+                    'offset': self._i_in,
+                    'opcode-byte': b
+                }
+                raise ValueError('unrecognised opcode', report)
+            self._i_in += 1
+            # writeout (like opcode execution)
+            # TODO: Implement optional halt policy that stops decoding
+            # when a line has too many bytes?
+            # We are currently using a discard/truncate policy for
+            # excess bytes.
+            for x in self._writeout(np=np+(248*npx), nr=nr, rb=rb, ub=ub):
+                if self._i_buf >= self.line_size: break
+                current_buf[self._i_buf] = x
+                yield x
+                self._i_buf += 1
+            np = 0
+            npx = 0
+            nr = 0
+            rb = ()
+            ub = ()
+            if self._i_buf >= self.line_size:
+                self._i_buf = 0
+                self._buffer = current_buf.copy() # PROTIP: cannot just assign
+
+# decoders for manual testing
+testdec8 = SCoADecoder(8, init_value=b'\x0f')
+testdec255 = SCoADecoder(255, init_value=b'\x0f')
+testdec1k = SCoADecoder(255, init_value=b'\x0f')
+
---
+++ tests/test_scoa.py
@@ -0,0 +1,173 @@
+"""
+SCoA Toolkit (scoa.py) Unit Tests
+"""
+# Written by Moses Chong
+# First edition 2022/05/16
+#
+# PUBLIC DOMAIN, NO RIGHTS RESERVED
+#
+# To the extent possible under law, the author(s) have dedicated all
+# copyright and related and neighboring rights to this software
+# to the public domain worldwide. This software is distributed
+# without any warranty.
+#
+# You should have received a copy of the CC0 Public Domain Dedication
+# along with this software. If not, see:
+# <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+from unittest import TestCase
+import scoa
+
+LINE_SIZE = 8
+LINE_SIZE_LONG = 1000
+
+class ScoaDecoderTests(TestCase):
+    # NOTE: The test data are interlaced with the test methods,
+    # not in a separate section
+
+    WRITEOUT_CASES = {
+        'old_only': {
+            'init_args': {'line_size': LINE_SIZE, 'init_value': b'\xf0'},
+            'test_args': {'np': 8},
+            'expected': b'\xf0\xf0\xf0\xf0\xf0\xf0\xf0\xf0',
+        },
+        'repeat_only': {
+            'init_args': {'line_size': LINE_SIZE, 'init_value': b'\xf0'},
+            'test_args': {'np': 0, 'nr': 8, 'rb': b'\xd0'},
+            'expected': b'\xd0\xd0\xd0\xd0\xd0\xd0\xd0\xd0',
+        },
+        'repeat_then_new': {
+            'init_args': {'line_size': LINE_SIZE, 'init_value': b'\xf0'},
+            'test_args': {'nr': 4, 'rb': b'\xde', 'ub': b'\x9a\x9b\x9c\x9d'},
+            'expected': b'\xde\xde\xde\xde\x9a\x9b\x9c\x9d',
+        },
+        'new_only': {
+            'init_args': {'line_size': LINE_SIZE, 'init_value': b'\xf0'},
+            'test_args': {'ub': b'\x9a\x9b\x9c\x9d\x9e\x9f\xaa\xab'},
+            'expected': b'\x9a\x9b\x9c\x9d\x9e\x9f\xaa\xab',
+        },
+        'old_then_repeat': {
+            'init_args': {'line_size': LINE_SIZE, 'init_value': b'\xf0'},
+            'test_args': {'np': 4, 'nr': 4, 'rb': b'\xd0'},
+            'expected': b'\xf0\xf0\xf0\xf0\xd0\xd0\xd0\xd0',
+        },
+        'old_then_new': {
+            'init_args': {'line_size': LINE_SIZE, 'init_value': b'\xf0'},
+            'test_args': {'np': 4, 'ub': b'\x9a\x9b\x9c\x9d'},
+            'expected': b'\xf0\xf0\xf0\xf0\x9a\x9b\x9c\x9d',
+        },
+    }
+
+    def test_writeout(self):
+        for t in self.WRITEOUT_CASES.values():
+            with self.subTest(test=t):
+                sd = scoa.SCoADecoder(**t['init_args'])
+                samp = bytes(sd._writeout(**t['test_args']))
+                self.assertEqual(samp, t['expected'])
+
+    # NOTE: For now, 'old' means copied from previous line,
+    # 'new' means new, uncompressed bytes from input and
+    # 'repeat' means repeated, new compressed bytes from input.
+    DECODE_CASES = {
+        'new': {
+            'init_args': {'line_size': LINE_SIZE, 'init_value': b'\xf0'},
+            'input': b'\x38\x00\x01\x02\x03\x04\x05\x06',
+            'expected': b'\x00\x01\x02\x03\x04\x05\x06',
+        },
+        'new_long': {
+            'init_args': {'line_size': LINE_SIZE_LONG, 'init_value': b'\xf0'},
+            'input': b''.join((
+                b'\xbf\xf8', b'\x0a'*255,
+                b'\xbf\xf8', b'\x0b'*255,
+                b'\xbf\xf8', b'\x0c'*255,
+                b'\xbd\xd8', b'\x0d'*235,
+            )),
+            'expected': b''.join((
+                b'\x0a'*255, b'\x0b'*255, b'\x0c'*255, b'\x0d'*235
+            ))
+        },
+        'repeat': {
+            'init_args': {'line_size': LINE_SIZE, 'init_value': b'\xf0'},
+            'input': b'\x78\x9a',
+            'expected': b'\x9a\x9a\x9a\x9a\x9a\x9a\x9a',
+        },
+        'repeat_long': {
+            'init_args': {'line_size': LINE_SIZE_LONG, 'init_value': b'\xf0'},
+            'input': b'\xbf\xb8\x9a\xbf\xb8\x9a\xbf\xb8\x9a\xbd\x98\x9a',
+            'expected': b'\x9a'*1000
+        },
+        'repeat_then_new': {
+            'init_args': {'line_size': LINE_SIZE, 'init_value': b'\xf0'},
+            'input': b'\xe4\x90\x01\x02\x03\x04',
+            'expected': b'\x90\x90\x90\x90\x01\x02\x03\x04',
+        },
+        'repeat_long_then_new': {
+            'init_args': {'line_size': LINE_SIZE_LONG, 'init_value': b'\xf0'},
+            'input': b'\xbf\xb8\x9a\xbf\xb8\x9a\xbf\xb8\x9a\xbc\x3c\x9a\x0a\x0b\x0c\x0d',
+            'expected': b''.join((b'\x9a'*996, b'\x0a\x0b\x0c\x0d'))
+        },
+        'old_then_new': {
+            'init_args': {'line_size': LINE_SIZE, 'init_value': b'\xf0'},
+            'input': b'\x78\x00\x08\x00\x24\xa0\xa1\xa2\xa3',
+            'expected': b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xa0\xa1\xa2\xa3',
+        },
+        'old_long_then_new': {
+            'init_args': {'line_size': LINE_SIZE, 'init_value': b'\xf0'},
+            'input': b'',
+            'expected': b''.join((b'\x00'*255, b'\xff'*745))
+        },
+        'old_then_repeat': {
+            'init_args': {'line_size': LINE_SIZE, 'init_value': b'\xf0'},
+            'input': b'\x78\x00\x08\x00\x64\xa0',
+            'expected': b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xa0\xa0\xa0\xa0',
+        },
+        'eol_full_line': {
+            'init_args': {'line_size': LINE_SIZE, 'init_value': b'\xf0'},
+            'input': b'\xe4\x9a\xa0\xa1\xa2\xa3\x41',
+            'expected': b'\x9a\x9a\x9a\x9a\xa0\xa1\xa2\xa3'*2
+        },
+        'eol_half_line': {
+            'init_args': {'line_size': LINE_SIZE, 'init_value': b'\xf0'},
+            'input': b'\xf9\x00\x00\xe0\xff\x41',
+            'expected': b'\x00\x00\x00\x00\x00\x00\x00\x00\xff\xff\xff\xff\x00\x00\x00\x00'
+        },
+        'eol_half_line_2x': {
+            'init_args': {'line_size': LINE_SIZE, 'init_value': b'\xf0'},
+            'input': b'\xf9\x00\x00\xe0\xff\x41\x41',
+            'expected': b''.join((b'\x00'*8, b'\xff\xff\xff\xff\x00\x00\x00\x00'*2))
+        }
+    }
+
+    def test_decode(self):
+        for k in self.DECODE_CASES.keys():
+            testdata = self.DECODE_CASES[k]
+            with self.subTest(test=k, input=testdata['input']):
+                sd = scoa.SCoADecoder(**testdata['init_args'])
+                samp = bytes(sd.decode(iter(testdata['input'])))
+                self.assertEqual(samp, testdata['expected'])
+
+    def test_decode_buffer_full_line(self):
+        """The buffer must hold a copy of the previous line"""
+        sd = scoa.SCoADecoder(LINE_SIZE, init_value=b'\xf0')
+        #biter = (x for x in b'\x60\x00\x20\x90\x91\x92\x93') # alt. version
+        biter = (x for x in b'\xe4\x00\x90\x91\x92\x93') 
+        [x for x in sd.decode(biter)]
+        self.assertEqual(bytes(sd._buffer), b'\x00\x00\x00\x00\x90\x91\x92\x93')
+
+    def test_decode_buffer_half_line(self):
+        """Buffer must not change until a whole line is written"""
+        sd = scoa.SCoADecoder(LINE_SIZE, init_value=b'\xf0')
+        biter = iter(b'\x50\x00') # 0x00 four times
+        [x for x in sd.decode(biter)]
+        # PROTIP: generators don't run until they are consumed,
+        # we need to create a comprehension to consume the generator
+        self.assertEqual(bytes(sd._buffer), b'\xf0\xf0\xf0\xf0\xf0\xf0\xf0\xf0')
+
+    def test_decode_buffer_overflow(self):
+        """Excess bytes must be discarded when buffer gets full"""
+        sd = scoa.SCoADecoder(LINE_SIZE, init_value=b'\xf0')
+        biter = iter(b'\x78\x90\x50\x91') # 0x90 seven times, 0x91 twice
+        out = [x for x in sd.decode(biter)]
+        self.assertEqual(bytes(out), b'\x90\x90\x90\x90\x90\x90\x90\x91')
+        self.assertEqual(bytes(sd._buffer), b'\x90\x90\x90\x90\x90\x90\x90\x91')
+
